d421a888345df27a46d89362019e4516115b5e78
eacazares <eacazar@gmail.com>
2022-10-18 21:29:12 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Milestone-Project-1
+
+The inspiration for this project is a classic, simple trivia game. The trivia theme will be based on a topic that it is enjoyable to me.
---
+++ index.js
@@ -0,0 +1,18 @@
+for(let i = 0; i < questions.length; i++){
+
+}
+
+const questions = [
+    {
+        prompt: 'What year was the original Star Wars movie released?',
+        
+        answers: {
+            A: '1979'
+            B: '1984'
+            C: '1977'
+            D: '1981'
+        }
+
+        
+    }
+]
